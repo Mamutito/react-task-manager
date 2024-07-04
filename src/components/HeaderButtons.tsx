267aa1a6f0797e4b5a29cacf12b053b338b86a26
Mamutito <aithor123@gmail.com>
2024-07-04 14:44:39 +0100

    chore: dropdown menu

--- src/components/HeaderButtons.tsx
+++ src/components/HeaderButtons.tsx
@@ -6,6 +6,7 @@ import Button from "./Button";
 import IconButton from "./IconButton";
 import UserProfileHeader from "./UserProfileHeader";
 import { useAppSelector } from "../store/hooks";
+import { Link } from "react-router-dom";
 
 const HeaderButtons: React.FC = () => {
   const user = useAppSelector((state) => state.users.currentUser);
@@ -15,7 +16,23 @@ const HeaderButtons: React.FC = () => {
       <IconButton Icon={MdAdd} className="block md:hidden" />
       <IconButton Icon={BsFillChatFill} ping />
       <IconButton Icon={FiList} />
-      <UserProfileHeader user={user} />
+      <section className="relative group">
+        <UserProfileHeader user={user} />
+        <div className="group-hover:block hidden absolute">
+          <ul className="w-full bg-white text-gray-700 rounded-md shadow-md min-w-max mt-5 p-1 font-semibold">
+            <li>
+              <Link className="hover:bg-gray-200 py-2 px-4 block" to="/profile">
+                Go to your profile
+              </Link>
+            </li>
+            <li>
+              <Link className="hover:bg-gray-200 py-2 px-4 block" to="/auth">
+                Logout
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </section>
     </section>
   );
 };
--- src/utils/formDate.ts
+++ src/utils/formDate.ts
@@ -1,10 +1,10 @@
 import { format } from "date-fns";
 
 export default function formatDate(input: string) {
-  const [datePart, timePart] = input.split(", ");
+  const [datePart] = input.split(", ");
   const [day, month, year] = datePart.split("/").map(Number);
 
   const date = new Date(year, month - 1, day); // Crear un objeto Date
 
-  return format(date, "eee do, MMMM, yyyy"); // Formatear la fecha
+  return format(date, "eee, MMM LL, yyyy"); // Formatear la fecha
 }
